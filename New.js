491c426dd7c6880c76feddf44612e9d421d088ad
ziyangxiao <1986200191@qq.com>
2019-12-05 13:44:35 +0800

    Update New.js

--- New.js
+++ New.js
@@ -1,11 +1,11 @@
 function New(fn) {
-    //new 规定返回新对象
+    //创建新对象
     const newObj = {}
     if(fn.prototype !== null) {
       // 新对象隐式原型链到构造函数的prototype属性
       newObj.__proto__ = fn.prototype
     }
-    //将this指向到当前创建的新对象
+    //将构造函数的作用域赋给新对象，this指向到当前创建的新对象
     const rtObj = fn.apply(newObj,Array.prototype.slice.call(arguments,1))
     //如果执行函数返回不是对象类型，那就返回当前对象的引用
     if((typeof rtObj === 'object' || typeof rtObj === 'function') && rtObj !== null) {
