dc0a92fd529dfd0d837d4f1d613f1b8755d7799e
ziyangxiao <1986200191@qq.com>
2019-12-05 10:38:13 +0800

    Create promise.js

---
+++ promise.js
@@ -0,0 +1,220 @@
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
+function Promise(executor) {
+    let self = this;
+    self.status = PENDING;
+    self.onFulfilled = [];//成功的回调
+    self.onRejected = []; //失败的回调
+    //PromiseA+ 2.1
+    function resolve(value) {
+        if (self.status === PENDING) {
+            self.status = FULFILLED;
+            self.value = value;
+            self.onFulfilled.forEach(fn => fn());//PromiseA+ 2.2.6.1
+        }
+    }
+
+    function reject(reason) {
+        if (self.status === PENDING) {
+            self.status = REJECTED;
+            self.reason = reason;
+            self.onRejected.forEach(fn => fn());//PromiseA+ 2.2.6.2
+        }
+    }
+
+    try {
+        executor(resolve, reject);
+    } catch (e) {
+        reject(e);
+    }
+}
+
+Promise.prototype.then = function (onFulfilled, onRejected) {
+    //PromiseA+ 2.2.1 / PromiseA+ 2.2.5 / PromiseA+ 2.2.7.3 / PromiseA+ 2.2.7.4
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+    onRejected = typeof onRejected === 'function' ? onRejected : reason => { throw reason };
+    let self = this;
+    //PromiseA+ 2.2.7
+    let promise2 = new Promise((resolve, reject) => {
+        if (self.status === FULFILLED) {
+            //PromiseA+ 2.2.2
+            //PromiseA+ 2.2.4 --- setTimeout
+            setTimeout(() => {
+                try {
+                    //PromiseA+ 2.2.7.1
+                    let x = onFulfilled(self.value);
+                    resolvePromise(promise2, x, resolve, reject);
+                } catch (e) {
+                    //PromiseA+ 2.2.7.2
+                    reject(e);
+                }
+            });
+        } else if (self.status === REJECTED) {
+            //PromiseA+ 2.2.3
+            setTimeout(() => {
+                try {
+                    let x = onRejected(self.reason);
+                    resolvePromise(promise2, x, resolve, reject);
+                } catch (e) {
+                    reject(e);
+                }
+            });
+        } else if (self.status === PENDING) {
+            self.onFulfilled.push(() => {
+                setTimeout(() => {
+                    try {
+                        let x = onFulfilled(self.value);
+                        resolvePromise(promise2, x, resolve, reject);
+                    } catch (e) {
+                        reject(e);
+                    }
+                });
+            });
+            self.onRejected.push(() => {
+                setTimeout(() => {
+                    try {
+                        let x = onRejected(self.reason);
+                        resolvePromise(promise2, x, resolve, reject);
+                    } catch (e) {
+                        reject(e);
+                    }
+                });
+            });
+        }
+    });
+    return promise2;
+}
+
+function resolvePromise(promise2, x, resolve, reject) {
+    let self = this;
+    //PromiseA+ 2.3.1
+    if (promise2 === x) {
+        reject(new TypeError('Chaining cycle'));
+    }
+    if (x && typeof x === 'object' || typeof x === 'function') {
+        let used; //PromiseA+2.3.3.3.3 只能调用一次
+        try {
+            let then = x.then;
+            if (typeof then === 'function') {
+                //PromiseA+2.3.3
+                then.call(x, (y) => {
+                    //PromiseA+2.3.3.1
+                    if (used) return;
+                    used = true;
+                    resolvePromise(promise2, y, resolve, reject);
+                }, (r) => {
+                    //PromiseA+2.3.3.2
+                    if (used) return;
+                    used = true;
+                    reject(r);
+                });
+
+            }else{
+                //PromiseA+2.3.3.4
+                if (used) return;
+                used = true;
+                resolve(x);
+            }
+        } catch (e) {
+            //PromiseA+ 2.3.3.2
+            if (used) return;
+            used = true;
+            reject(e);
+        }
+    } else {
+        //PromiseA+ 2.3.3.4
+        resolve(x);
+    }
+}
+
+// Promise.resolve(value) 返回一个以给定值解析后的Promise 对象.
+
+// 如果 value 是个 thenable 对象，返回的promise会“跟随”这个thenable的对象，采用它的最终状态
+// 如果传入的value本身就是promise对象，那么Promise.resolve将不做任何修改、原封不动地返回这个promise对象。
+// 其他情况，直接返回以该值为成功状态的promise对象。
+Promise.resolve = function (param) {
+    if (param instanceof Promise) {
+    return param;
+}
+return new Promise((resolve, reject) => {
+    if (param && param.then && typeof param.then === 'function') {
+        setTimeout(() => {
+            param.then(resolve, reject);
+        });
+    } else {
+        resolve(param);
+    }
+});
+}
+
+//Promise.reject方法和Promise.resolve不同，Promise.reject()方法的参数，会原封不动地作为reject的理由，变成后续方法的参数。
+Promise.reject = function (reason) {
+    return new Promise((resolve, reject) => {
+        reject(reason);
+    });
+}
+// Promise.prototype.catch 用于指定出错时的回调，是特殊的then方法，catch之后，可以继续 .then
+Promise.prototype.catch = function (onRejected) {
+    return this.then(null, onRejected);
+}
+
+//不管成功还是失败，都会走到finally中,并且finally之后，还可以继续then。并且会将值原封不动的传递给后面的then.
+Promise.prototype.finally = function (callback) {
+    return this.then((value) => {
+        return Promise.resolve(callback()).then(() => {
+            return value;
+        });
+    }, (err) => {
+        return Promise.resolve(callback()).then(() => {
+            throw err;
+        });
+    });
+}
+
+
+// Promise.all(promises) 返回一个promise对象
+
+// 如果传入的参数是一个空的可迭代对象，那么此promise对象回调完成(resolve),只有此情况，是同步执行的，其它都是异步返回的。
+// 如果传入的参数不包含任何 promise，则返回一个异步完成.
+// promises 中所有的promise都promise都“完成”时或参数中不包含 promise 时回调完成。
+// 如果参数中有一个promise失败，那么Promise.all返回的promise对象失败
+// 在任何情况下，Promise.all 返回的 promise 的完成状态的结果都是一个数组
+
+Promise.all = function (promises) {
+    return new Promise((resolve, reject) => {
+        let index = 0;
+        let result = [];
+        if (promises.length === 0) {
+            resolve(result);
+        } else {
+            function processValue(i, data) {
+                result[i] = data;
+                if (++index === promises.length) {
+                    resolve(result);
+                }
+            }
+            for (let i = 0; i < promises.length; i++) {
+                  //promises[i] 可能是普通值
+                  Promise.resolve(promises[i]).then((data) => {
+                    processValue(i, data);
+                }, (err) => {
+                    reject(err);
+                    return;
+                });
+            }
+        }
+    });
+}
+
+Promise.defer = Promise.deferred = function () {
+    let dfd = {};
+    dfd.promise = new Promise((resolve, reject) => {
+        dfd.resolve = resolve;
+        dfd.reject = reject;
+    });
+    return dfd;
+}
+
+
+module.exports = Promise;
